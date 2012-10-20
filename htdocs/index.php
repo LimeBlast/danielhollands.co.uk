d2a8b1720990d9ca2b9fbb6f39103a816b5c3307
LimeBlast <daniel@limeblast.co.uk>
2012-10-20 20:30:47 +0100

    Made a start on importing content

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 	<head>
 		<meta charset="utf-8">
-		<title>Daniel Hollands</title>
+		<title>Daniel Hollands - Web developer's curriculum vitae (CV) and portfolio</title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width">
 
@@ -18,8 +18,163 @@
 		<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
 		<![endif]-->
 
-		<!-- Add your site or application content here -->
-		<p>Hello world! This is HTML5 Boilerplate.</p>
+		<div id="menu" class="menu">
+			<header id="header" class="header" role="banner">
+				<hgroup>
+					<h1>Daniel Hollands</h1>
+					<h2>Web developer's curriculum vitae (CV) and portfolio</h2>
+				</hgroup>
+			</header><!-- #header -->
+			<nav id="nabigation" class="navigation" role="navigation">
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#about">About Me</a></li>
+					<li><a href="#skills">Key Skills</a></li>
+					<li><a href="#education">Education</a></li>
+					<li><a href="#employment">Employment</a></li>
+					<li><a href="#portfolio">Portfolio</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav><!-- #navigation -->
+		</div><!-- #menu -->
+
+		<div id="content" class="content">
+			<article id="main" class="main" role="main">
+
+				<section id="home" class="home">
+
+					<p>I'm a Web Developer that has a passion for the World Wide Web.</p>
+
+				</section><!-- #home -->
+
+				<section id="about" class="about">
+
+					<h2>About Me</h2>
+
+					<p>I believe the Web is the greatest thing the human race has ever developed. As such, I'm proud to contribute towards its ongoing development by building quality Web sites.</p>
+					<p>I have a keen interest in learning and self development, and where possible, I like to employ the scientific method to everything that I do, making choices based on research and evidence.</p>
+
+				</section><!-- #home -->
+
+				<section id="skills" class="skills">
+
+					<h2>Key Skills</h2>
+
+					<ul>
+						<li>Professional experience of tools and technologies including <abbr title="PHP: Hypertext Preprocessor">PHP</abbr> 5.3, <abbr title="My Structure Query Language">MySQL</abbr> 5, <abbr title="Hyper Text Markup Language">HTML</abbr> 5, <abbr title="Cascading Style Sheets">CSS</abbr> 3, and jQuery 1.7, which I use for the creation of dynamic Web sites.</li>
+						<li>Experience using the <abbr title="Model View Controller">MVC</abbr> frameworks <em>CakePHP</em> and <em>Fuel</em>, the content management systems <em>Wordpress</em>, <em>Perch CMS</em> and <em>CouchCMS</em>, and the e-commerce platform <em>Lemonstand</em>.</li>
+						<li>Knowledge of Web standards, allowing for the creation of sites that work across multiple browsers and devices, while ensuring they comply with accessibility legislation.</li>
+						<li>Experience with usability evaluation techniques, which are useful for testing a site's ease of use, and highlighting where improvements could be made.</li>
+						<li>Experience installing, configuring and running a Linux (Debian) Web server, and using Git version control software.</li>
+					</ul>
+
+				</section><!-- #skills -->
+
+				<section id="education" class="education">
+
+					<h2>Education</h2>
+
+					<h3>University of Worcester</h3>
+					<h4>First Class Honours in Web Development (BSc)</h4>
+					<p>I first gained an interest in the World Wide Web as a hobby in 1999. I spent much of my spare time learning Web design and development techniques, and as time progressed, and my skills improved, I decided to look for work in the Web industry. I found that finding work, despite my self-taught skills, difficult due to a lack of qualifications, which prompted me to go to university.</p>
+					<p>My time at university was a vastly positive time in my life, and allowed me to improve upon my existing design and development skills, in addition to learning project management, planning and research techniques. Among the modules studied, I particularly enjoyed:</p>
+					<ul>
+						<li><strong>Advanced Web Design</strong>, which covered advanced XHTML and CSS techniques, in addition to Web standards and cross browser compatibility, allowing for maximum Web exposure.</li>
+						<li><strong>Qualitative Interface Evaluation</strong>, which covered interface design and usability evaluation, allowing for the creation of sites that are both attractive and easy to use.</li>
+						<li><strong>Publishing on the Web</strong>, which covered a range of Web publishing mediums, such as blogs and social networking sites, and how to use them effectively.</li>
+						<li><strong>Object Oriented Programming</strong>, which taught the fundamentals of OOP, and allowed me to vastly improve my PHP development skills.</li>
+					</ul>
+					<p>I've also been <a href="http://www.worcester.ac.uk/community/daniel-hollands.html">featured as a success story</a> on the University of Worcester's Web site.</p>
+
+					<h4>Worcester Ideas Network</h4>
+					<p>In my third year at university I was voted into the position of chairperson for the entrepreneurial society Worcester Ideas Network (WIN). The society helps students by organising workshops and participating in networking and competitions across Europe. One such competition was EPOCH, hosted by the London School of Economics in the style of The Apprentice TV show. My team won the marketing challenge which was judged by Orange, and continued to come second place in the competition overall.</p>
+					<p>My time as chairperson of the society provided me with valuable leadership and planning experience, as demonstrated in the planning of a trip to Germany to participate in an international conference, and the organisation of a series of seminars on idea generation, presented by lecturers from other universities.</p>
+
+				</section><!-- #education -->
+
+				<section id="employment" class="employment">
+
+					<h2>Employment</h2>
+
+					<h2>Web Developer</h2>
+					<ul>
+						<li>Ghost Design Consultants Ltd. (Leamington Spa)</li>
+						<li>Sep 2011 - Present</li>
+					</ul>
+					<p>I am responsible for the development of all the Web elements of projects taken on by the company. This includes writing project proposals and specifications, frontend coding of Photoshop designs, backend development using the tools best suited to the project, and leasing with clients.</p>
+
+					<h2>Backend PHP Developer</h2>
+					<ul>
+						<li>Propeller Communications (Rugby)</li>
+						<li>Oct 2010 - Sep 2011</li>
+					</ul>
+					<p>My role consisted of development with the company's MVC-based framework to build dynamic PHP-based CMS Web sites. This role included management of Mercurial repositories, which provides a safety-net during development, and allows multiple members of a team to work in the same project. The role has always allowed me to gain experience with jQuery and the use of AJAX, to provide a seamless end user experience.</p>
+
+					<h2>Graduate Intern Web Developer</h2>
+					<ul>
+						<li>South Worcestershire College (Evesham)</li>
+						<li>Jan 2010 - Sep 2010</li>
+					</ul>
+					<p>I was solely responsible for the planning, development, testing, launch and maintenance of a new college Web site. The focus of the new site was that of making it easier to use, to improve compliance with the Disability Discrimination Act, and to improve search engine rankings. Upon completion it was greeted with positive feedback from both staff and students at the college, and an increased conversion rate.</p>
+
+					<h2>IT and Media Support Technician</h2>
+					<ul>
+						<li>University of Worcester</li>
+						<li>Sep 2008 - Dec 2009</li>
+					</ul>
+					<p>I was part of a team responsible for providing media and technical support to students within the university's library. The role involved supporting the university's PCs, printers and software, as well as helping student's laptops to connect to the network. We needed good customer service skills, to ensure we dealt with customers with courtesy and respect, and as we were the most senior members of staff during our shifts, we had to uphold a professional image of the library and its ensure compliance of its code of conduct.</p>
+
+					<h2>Technical Support Technician</h2>
+					<ul>
+						<li>Fujitsu / Tesco Broadband (Sidcup)</li>
+						<li>Aug 2005 - Mar 2006</li>
+					</ul>
+					<p>This call centre based role consisted of providing support to customers experiencing trouble using their broadband connection. It required a good telephone manner and the ability to explain complex instructions in an easy to understand way.</p>
+
+				</section><!-- #employment -->
+
+				<section id="portfolio" class="portfolio">
+
+					<h2>Portfolio</h2>
+					<p>No idea yet, I'll have to think about it.</p>
+
+				</section><!-- #portfolio -->
+
+				<section id="contact" class="contact">
+
+					<h2>Contact</h2>
+					<form method="post">
+						<fieldset>
+							<label for="name">Name</label>
+							<input type="text" id="name" name="name" />
+							<label for="email">Email</label>
+							<input type="text" id="email" name="email" />
+							<label for="message">Message</label>
+							<textarea id="message" name="message"></textarea>
+							<button type="submit">Send</button>
+						</fieldset>
+					</form>
+					<dl>
+						<dt>Mobile</dt>
+						<dd>07887 521 229</dd>
+						<dt>Email</dt>
+						<dd>daniel@limeblast.co.uk</dd>
+						<dt>Blog</dt>
+						<dd><a href="http://limeblast.co.uk/">Lime Blast</a></dd>
+						<dt>Twitter</dt>
+						<dd><a href="http://twitter.com/limeblast/">@LimeBlast</a></dd>
+						<dt>Facebook</dt>
+						<dd><a href="http://facebook.com/limeblast/">LimeBlast</a></dd>
+					</dl>
+
+				</section><!-- #contact -->
+
+			</article><!-- #main -->
+
+			<footer id="footer" class="footer" role="contentinfo">
+
+			</footer><!-- #footer -->
+		</div><!-- #content -->
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="/js/vendor/jquery-1.8.0.min.js"><\/script>')</script>
