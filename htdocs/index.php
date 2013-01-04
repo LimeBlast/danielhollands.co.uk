2500c947733b249542f8a87c8638e43e2cc82662
LimeBlast <daniel@limeblast.co.uk>
2013-01-04 22:25:00 +0000

    Lots of changes, more than I realy care to count right now..

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,11 +3,31 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
+	<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# profile: http://ogp.me/ns/profile#">
 		<meta charset="utf-8">
-		<title>Daniel Hollands - Web developer's curriculum vitae (CV) and portfolio</title>
-		<meta name="description" content="">
+		<title>My name is Daniel - a Web developer's curriculum vitae (CV) and portfolio</title>
+		<meta name="description" content="I am a Web Developer with a passion for the World Wide Web and this is my curriculum vitae and portfolio.">
 		<meta name="viewport" content="width=device-width">
+
+		<!-- Open Graph: http://ogp.me/ -->
+		<meta property="og:title" content="My name is Daniel" />
+		<meta property="og:description" content="I am a Web Developer with a passion for the World Wide Web and this is my curriculum vitae and portfolio." />
+		<meta property="og:url" content="http://danielhollands.co.uk/" />
+		<meta property="og:image" content="http://danielhollands.co.uk/img/daniel-hollands.png" />
+		<meta property="og:locale" content="en_GB" />
+		<meta property="og:type" content="profile" />
+		<meta property="profile:first_name" content="Daniel" />
+		<meta property="profile:last_name" content="Hollands" />
+		<meta property="profile:username" content="LimeBlast" />
+		<meta property="profile:gender" content="male" />
+
+		<!-- Twitter Card: https://dev.twitter.com/docs/cards -->
+		<meta name="twitter:card" content="summary">
+		<meta name="twitter:creator" content="@limeblast">
+		<meta name="twitter:url" content="http://danielhollands.co.uk/">
+		<meta name="twitter:title" content="My name is Daniel">
+		<meta name="twitter:description" content="I am a Web Developer with a passion for the World Wide Web and this is my curriculum vitae and portfolio.">
+		<meta name="twitter:image" content="http://danielhollands.co.uk/img/daniel-hollands.png">
 
 		<link rel="stylesheet" href="/css/main.css">
 		<script src="/js/vendor/modernizr-2.6.1.min.js"></script>
@@ -25,7 +45,7 @@
 						<li><a href="#skills">Key Skills</a></li>
 						<li><a href="#education">Education</a></li>
 						<li><a href="#employment">Employment</a></li>
-						<li><a href="#portfolio">Portfolio</a></li>
+<!--						<li><a href="#portfolio">Portfolio</a></li> -->
 						<li><a href="#contact">Contact</a></li>
 					</ul>
 				</nav><!-- #navigation -->
@@ -37,7 +57,7 @@
 					<header id="header" class="header" role="banner">
 
 						<h1>My name is Daniel.</h1>
-						<div><img src="/img/header.png" alt="" /></div>
+						<div class="hero-shot"><img src="/img/header.png" alt="" /></div>
 						<p><span class="slabtext">I am a Web Developer</span> <span class="slabtext">with a passion for the</span> <span class="slabtext">World Wide Web.</span></p>
 						<p><span class="slabtext">Welcome to my</span> <span class="slabtext">curriculum vitae</span> <span class="slabtext">and portfolio.</span></p>
 
@@ -47,9 +67,11 @@
 
 						<h2>About Me</h2>
 
-						<div>
-							<p>I believe the Web is the greatest thing the human race has ever developed. As such, I'm proud to contribute towards its ongoing development by building quality Web sites.</p>
-							<p>I have a keen interest in learning and self development, and where possible, I like to employ the scientific method to everything that I do, making choices based on research and evidence.</p>
+						<div class="text">
+							<p>I believe the World Wide Web is the greatest thing the human race has ever developed, and I'm proud to contribute towards its ongoing development by building quality Web sites and promoting accessibility and usability.</p>
+							<p>I graduated from the University of Worcester in 2010, obtaining a first class honours degree, which I feel is the greatest achievement of my life (so far).</p>
+							<p>I have a keen interest in self development and I like to employ the scientific method to everything that I do by making choices based on research and evidence.</p>
+							<p>In my spare time I enjoy experimenting with new technology, playing trading card and computer games, and exploring the outside world, either by bicycle or on foot.</p>
 						</div>
 
 					</section><!-- #home -->
@@ -58,7 +80,7 @@
 
 						<h2>Key Skills</h2>
 
-						<ul>
+						<ul class="skills-list">
 							<li class="coding">
 								<h3>Coding</h3>
 								<p>Professional experience of coding tools and technologies including <abbr title="PHP: Hypertext Preprocessor">PHP</abbr>, <abbr title="My Structure Query Language">MySQL</abbr>, <abbr title="Hyper Text Markup Language version 5">HTML 5</abbr>, <abbr title="Cascading Style Sheets level 3">CSS 3</abbr>, and jQuery, all of which allow for the creation of dynamic Web sites.</p>
@@ -92,10 +114,10 @@
 						<h2>Education</h2>
 
 						<h3>University of Worcester</h3>
-						<h4>First Class Honours in Web Development (BSc)</h4>
+						<h4>First Class Honours Degree in Web Development (BSc)</h4>
 						<p>I first gained an interest in the World Wide Web as a hobby in 1999. I spent much of my spare time learning Web design and development techniques, and as time progressed, and my skills improved, I decided to look for work in the Web industry. I found that finding work, despite my self-taught skills, difficult due to a lack of qualifications, which prompted me to go to university.</p>
 						<p>My time at university was a vastly positive time in my life, and allowed me to improve upon my existing design and development skills, in addition to learning project management, planning and research techniques. Among the modules studied, I particularly enjoyed:</p>
-						<ul>
+						<ul class="education-modules">
 							<li><strong>Advanced Web Design</strong>, which covered advanced XHTML and CSS techniques, in addition to Web standards and cross browser compatibility, allowing for maximum Web exposure.</li>
 							<li><strong>Qualitative Interface Evaluation</strong>, which covered interface design and usability evaluation, allowing for the creation of sites that are both attractive and easy to use.</li>
 							<li><strong>Publishing on the Web</strong>, which covered a range of Web publishing mediums, such as blogs and social networking sites, and how to use them effectively.</li>
@@ -104,7 +126,7 @@
 						<p>I've also been <a href="http://www.worcester.ac.uk/community/daniel-hollands.html">featured as a success story</a> on the University of Worcester's Web site.</p>
 
 						<h4>Worcester Ideas Network</h4>
-						<p><img src="/img/worcester-ideas-network.png" alt="Worcester Ideas Network logo" />In my third year at university I was voted into the position of chairperson for the entrepreneurial society Worcester Ideas Network (WIN). The society helps students by organising workshops and participating in networking and competitions across Europe. One such competition was EPOCH, hosted by the London School of Economics in the style of The Apprentice TV show. My team won the marketing challenge which was judged by Orange, and continued to come second place in the competition overall.</p>
+						<p><img src="/img/worcester-ideas-network.png" alt="Worcester Ideas Network logo" class="win-logo" />In my third year at university I was voted into the position of chairperson for the entrepreneurial society Worcester Ideas Network (WIN). The society helps students by organising workshops and participating in networking and competitions across Europe. One such competition was EPOCH, hosted by the London School of Economics in the style of The Apprentice TV show. My team won the marketing challenge which was judged by Orange, and continued to come second place in the competition overall.</p>
 						<p>My time as chairperson of the society provided me with valuable leadership and planning experience, as demonstrated in the planning of a trip to Germany to participate in an international conference, and the organisation of a series of seminars on idea generation, presented by lecturers from other universities.</p>
 
 					</section><!-- #education -->
@@ -113,52 +135,77 @@
 
 						<h2>Employment</h2>
 
-						<h3>Web Developer</h3>
-						<ul>
-							<li>Ghost Design Consultants Ltd. (Leamington Spa)</li>
-							<li>Sep 2011 - Present</li>
-							<li>Wordpress, CouchCMS, PerchCMS, Lemonstand, CakePHP</li>
+						<h3 class="employment-role">Web Developer</h3>
+						<ul class="employment-details">
+							<li class="employment-company">Ghost Design Consultants Ltd. (Leamington Spa)</li>
+							<li class="employment-dates">Sep 2011 - Present</li>
 						</ul>
-						<p>I am responsible for the development of all the Web elements of projects taken on by the company. This includes writing project proposals and specifications, frontend coding of Photoshop designs, backend development using the tools best suited to the project, and liaising with clients.</p>
-
-						<h3>Backend PHP Developer</h3>
-						<ul>
-							<li>Propeller Communications (Rugby)</li>
-							<li>Oct 2010 - Sep 2011</li>
-							<li>PHP, MySQL, Fuel, CodeIgniter</li>
+						<ul class="employment-tags">
+							<li>Wordpress</li>
+							<li>CouchCMS</li>
+							<li>PerchCMS</li>
+							<li>Lemonstand</li>
+							<li>CakePHP</li>
+							<li>Project Management</li>
 						</ul>
-						<p>My role consisted of development with the company's MVC-based framework to build dynamic PHP-based CMS Web sites. This role included management of Mercurial repositories, which provides a safety-net during development, and allows multiple members of a team to work on the same project. The role has also allowed me to gain experience with jQuery and the use of AJAX, to provide a seamless end user experience.</p>
+						<p class="employment-description">I am responsible for the development of all the Web elements of projects taken on by the company. This includes writing project proposals and specifications, frontend coding of Photoshop designs, backend development using the tools best suited to the project, and liaising with clients.</p>
 
-						<h3>Graduate Intern Web Developer</h3>
-						<ul>
-							<li>South Worcestershire College (Evesham)</li>
-							<li>Jan 2010 - Sep 2010</li>
-							<li>CakePHP, XHTML, CSS</li>
+						<h3 class="employment-role">Backend PHP Developer</h3>
+						<ul class="employment-details">
+							<li class="employment-company">Propeller Communications (Rugby)</li>
+							<li class="employment-dates">Oct 2010 - Sep 2011</li>
 						</ul>
-						<p>I was solely responsible for the planning, development, testing, launch and maintenance of a new college Web site. The focus of the new site was that of making it easier to use, to improve compliance with the Disability Discrimination Act, and to improve search engine rankings. Upon completion it was greeted with positive feedback from both staff and students at the college, and an increased conversion rate.</p>
-
-						<h3>IT and Media Support Technician</h3>
-						<ul>
-							<li>University of Worcester</li>
-							<li>Sep 2008 - Dec 2009</li>
-							<li>Windows, Networking</li>
+						<ul class="employment-tags">
+							<li>PHP</li>
+							<li>MySQL</li>
+							<li>Fuel</li>
+							<li>CodeIgniter</li>
 						</ul>
-						<p>I was part of a team responsible for providing media and technical support to students within the university's library. The role involved supporting the university's PCs, printers and software, as well as helping student's laptops to connect to the network. We needed good customer service skills, to ensure we dealt with customers with courtesy and respect, and as we were the most senior members of staff during our shifts, we had to uphold a professional image of the library and ensure compliance of its code of conduct.</p>
+						<p class="employment-description">My role consisted of development with the company's MVC-based framework to build dynamic PHP-based CMS Web sites. This role included management of Mercurial repositories, which provides a safety-net during development, and allows multiple members of a team to work on the same project. The role has also allowed me to gain experience with jQuery and the use of AJAX, to provide a seamless end user experience.</p>
 
-						<h3>Technical Support Technician</h3>
-						<ul>
-							<li>Fujitsu / Tesco Broadband (Sidcup)</li>
-							<li>Aug 2005 - Mar 2006</li>
+						<h3 class="employment-role">Graduate Intern Web Developer</h3>
+						<ul class="employment-details">
+							<li class="employment-company">South Worcestershire College (Evesham)</li>
+							<li class="employment-dates">Jan 2010 - Sep 2010</li>
+						</ul>
+						<ul class="employment-tags">
+							<li>CakePHP</li>
+							<li>XHTML</li>
+							<li>CSS</li>
+							<li>Project Management</li>
+						</ul>
+						<p class="employment-description">I was solely responsible for the planning, development, testing, launch and maintenance of a new college Web site. The focus of the new site was that of making it easier to use, to improve compliance with the Disability Discrimination Act, and to improve search engine rankings. Upon completion it was greeted with positive feedback from both staff and students at the college, and an increased conversion rate.</p>
+
+						<h3 class="employment-role">IT and Media Support Technician</h3>
+						<ul class="employment-details">
+							<li class="employment-company">University of Worcester</li>
+							<li class="employment-dates">Sep 2008 - Dec 2009</li>
+						</ul>
+						<ul class="employment-tags">
+							<li>Windows</li>
+							<li>Networking</li>
+							<li>Public Facing</li>
+						</ul>
+						<p class="employment-description">I was part of a team responsible for providing media and technical support to students within the university's library. The role involved supporting the university's PCs, printers and software, as well as helping student's laptops to connect to the network. We needed good customer service skills, to ensure we dealt with customers with courtesy and respect, and as we were the most senior members of staff during our shifts, we had to uphold a professional image of the library and ensure compliance of its code of conduct.</p>
+
+						<h3 class="employment-role">Technical Support Technician</h3>
+						<ul class="employment-details">
+							<li class="employment-company">Fujitsu / Tesco Broadband (Sidcup)</li>
+							<li class="employment-dates">Aug 2005 - Mar 2006</li>
+						</ul>
+						<ul class="employment-tags">
 							<li>ADLS</li>
+							<li>Technical Support</li>
+							<li>Telephone Manner</li>
 						</ul>
-						<p>This call centre based role consisted of providing support to customers experiencing trouble using their broadband connection. It required a good telephone manner and the ability to explain complex instructions in an easy to understand way.</p>
+						<p class="employment-description">This call centre based role consisted of providing support to customers experiencing trouble using their broadband connection. It required a good telephone manner and the ability to explain complex instructions in an easy to understand way.</p>
 
 					</section><!-- #employment -->
 
-					<section id="portfolio" class="portfolio">
+<!--					<section id="portfolio" class="portfolio">
 
 						<h2>Portfolio</h2>
-						<p>No idea yet, I'll have to think about it.</p>
+						<p>Hidden, for the moment, will appear soon.</p>
 
 					</section><!-- #portfolio -->
 
@@ -202,7 +249,7 @@
 				</article><!-- #main -->
 
 				<footer id="footer" class="footer" role="contentinfo">
-
+					<p>Site content copyright Daniel Hollands. <a href="/humans.txt">humans.txt</a>. <a href="https://github.com/LimeBlast/danielhollands.co.uk">View source on GitHub</a>.</p>
 				</footer><!-- #footer -->
 			</div><!-- #content -->
 		</div><!-- #container -->
@@ -212,11 +259,27 @@
 		<script src="/js/plugins.js"></script>
 		<script src="/js/main.js"></script>
 
-		<script>
-			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-			s.parentNode.insertBefore(g,s)}(document,'script'));
+		<script type="text/javascript">
+			var _gas = _gas || [];
+			_gas.push(['_setAccount', 'UA-27778608-1']); // REPLACE WITH YOUR GA NUMBER
+			_gas.push(['_setDomainName', '.danielhollands.co.uk']); // REPLACE WITH YOUR DOMAIN
+			_gas.push(['_trackPageview']);
+			_gas.push(['_gasTrackForms']);
+			_gas.push(['_gasTrackOutboundLinks']);
+			_gas.push(['_gasTrackMaxScroll']);
+			_gas.push(['_gasTrackDownloads']);
+			_gas.push(['_gasTrackYoutube', {force: true}]);
+			_gas.push(['_gasTrackVimeo', {force: true}]);
+			_gas.push(['_gasTrackMailto']);
+
+			(function() {
+				var ga = document.createElement('script');
+				ga.type = 'text/javascript';
+				ga.async = true;
+				ga.src = '//cdnjs.cloudflare.com/ajax/libs/gas/1.10.1/gas.min.js';
+				var s = document.getElementsByTagName('script')[0];
+				s.parentNode.insertBefore(ga, s);
+			})();
 		</script>
 	</body>
 </html>
