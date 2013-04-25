33adae75639e78f099b940b7472aa289d95e893a
LimeBlast <daniel@ghostds.com>
2013-04-25 14:04:41 +0100

    Made a few updates here and there

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,11 @@
+// http://stackoverflow.com/a/8414666/1049688 (only use for after-font effects)
+jQuery(window).bind("load", function() {
+	// slab text
+	jQuery('.header-title').slabText();
+	jQuery('.header-copy').slabText();
+});
+
+
 $(document).ready(function() {
 
 });
