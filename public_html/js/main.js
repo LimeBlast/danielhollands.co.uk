164d66ad3dae7259d74a08643a3b917564dac6eb
LimeBlast <daniel@ghostds.com>
2013-05-01 12:01:47 +0100

    Removed the animation effect from the shownav, but it should work better on more devices now

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,33 +1,38 @@
 // http://stackoverflow.com/a/8414666/1049688 (only use for after-font effects)
-jQuery(window).bind("load", function() {
+jQuery(window).bind("load", function () {
 	// slab text
 	jQuery('.header-title').slabText();
 	jQuery('.header-copy').slabText();
 });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	// one page nav
 	$('#navigation').onePageNav({
 		scrollOffset: 50,
-		begin: function() {
+		begin: function () {
 			//Hack so you can click other menu items after the initial click
 			$('body').append('<div id="device-dummy" style="height: 1px;"></div>');
 		},
-		end: function() {
+		end: function () {
 			$('#device-dummy').remove();
 		}
 	});
 
 	// show navigation
-	$('#shownavbutton').on('click', function(){
-		$('#shownav').slideToggle();
+	$('#shownavbutton').on('click', function () {
+		toggleNav();
 	});
 
 	// hide navigation when a link is clicked on
-	$('#navigation a').on('click', function(){
-		$('#shownav').slideToggle();
+	$('#navigation a').on('click', function () {
+		toggleNav();
 	});
+
+	// set-up slide toggle function
+	function toggleNav() {
+		$("#shownav").toggleClass("show");
+	}
 
 });
