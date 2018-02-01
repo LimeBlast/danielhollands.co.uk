6babcf1c2820036fa28d84f1ccfe7e5a36c5129a
Daniel Hollands <daniel@limeblast.co.uk>
2018-02-01 18:51:12 +0000

    General fixes here, there, and everywhere

--- Gemfile
+++ Gemfile
@@ -5,13 +5,7 @@ source 'http://rubygems.org'
 gem 'middleman', '~>3.3.4'
 
 # Live-reloading plugin
-gem 'middleman-livereload', '~> 3.1.0'
-
-# For faster file watcher updates on Windows:
-gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem 'tzinfo-data', platforms: [:mswin, :mingw]
+gem 'middleman-livereload', '~> 3.4.0'
 
 gem 'compass', '0.12.4'
 
--- source/js/main.js
+++ source/js/main.js
@@ -6,40 +6,40 @@
 
 // http://stackoverflow.com/a/8414666/1049688 (only use for after-font effects)
 jQuery(window).bind("load", function () {
-	// slab text
-	jQuery('.header-title').slabText();
-	jQuery('.header-copy').slabText();
+  // slab text
+  jQuery('.header-title').slabText();
+  jQuery('.header-copy').slabText();
 });
 
 
 $(document).ready(function () {
 
-	// one page nav
-	$('#navigation').onePageNav({
-		scrollOffset: 50,
-		begin: function () {
-			//Hack so you can click other menu items after the initial click
-			$('body').append('<div id="device-dummy" style="height: 1px;"></div>');
-		},
-		end: function () {
-			$('#device-dummy').remove();
-		},
-		filter: ':not(.external)'
-	});
+  // one page nav
+  $('#navigation').onePageNav({
+    scrollOffset: 50,
+    begin: function () {
+      //Hack so you can click other menu items after the initial click
+      $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
+    },
+    end: function () {
+      $('#device-dummy').remove();
+    },
+    filter: ':not(.external)'
+  });
 
-	// show navigation
-	$('#shownavbutton').on('click', function () {
-		toggleNav();
-	});
+  // show navigation
+  $('#shownavbutton').on('click', function () {
+    toggleNav();
+  });
 
-	// hide navigation when a link is clicked on
-	$('#navigation a').on('click', function () {
-		toggleNav();
-	});
+  // hide navigation when a link is clicked on
+  $('#navigation a').on('click', function () {
+    toggleNav();
+  });
 
-	// set-up slide toggle function
-	function toggleNav() {
-		$("#shownav").toggleClass("show");
-	}
+  // set-up slide toggle function
+  function toggleNav() {
+    $("#shownav").toggleClass("show");
+  }
 
 });
